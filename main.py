664656ee97108d2683a2802a8e0876661ff83b5b
Alex-tolch <tolch1999@mail.ru>
2026-02-22 20:39:36 +0500

    fix linter

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import sentry_sdk
 
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
 )
 
